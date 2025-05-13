a3b4cd5c40387e9fe09f7ce88632ac09d334909f
Sanjiv Paul <paulsanjiv81@gmail.com>
2025-05-13 16:08:38 +0530

    tag update and delete controller added

--- src/routes/tag/tag.routes.js
+++ src/routes/tag/tag.routes.js
@@ -2,8 +2,10 @@
 import express from "express";
 import {
   createTag,
+  deleteTag,
   getAllTags,
   getTagArticles,
+  updateTag,
 } from "../../controllers/tag/tag.controller.js";
 import { isAdmin, verifyJwt } from "../../middlewares/auth.middleware.js";
 
@@ -14,10 +16,10 @@ router.get("/", getAllTags);
 router.get("/:tagId/articles", getTagArticles);
 
 // Admin protected routes
-// router.use(verifyJwt, isAdmin);
-router.use(verifyJwt);
+router.use(verifyJwt, isAdmin);
+// router.use(verifyJwt);
 router.post("/", createTag);
-// router.patch("/:tagId", updateTag);
-// router.delete("/:tagId", deleteTag);
+router.patch("/:tagId", updateTag);
+router.delete("/:tagId", deleteTag);
 
 export default router;
